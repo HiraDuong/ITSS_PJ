79c258b6a6f0c3e6f3123cd77b56eb5dd3712781
Vũ Huy Hoàng <hirakuminamoto@yahoo.com>
2024-07-08 16:06:06 +0700

    Delete routes directory

--- routes/MqttRoute.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-
-const MqttController = require('../controller/MqttController');
-router.post('/mqtt', MqttController.sendmessage);
-
-module.exports = router;
